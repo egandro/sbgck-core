554fb1d9d8f551ac1df3c529a639e303819fee4f
Harald Fielker <harald.fielker@gmail.com>
2021-05-26 19:46:55 +0200

    .

--- src/sbgck.cpp
+++ src/sbgck.cpp
@@ -2,26 +2,34 @@
 
 using namespace SBGCK;
 
-bool Engine::init(string applicationDir, string url) {
+void Engine::queryTokens(QueryTokenParam &param, QueryTokenResult &result)
+{
+    Log(typelog::INFO) << "Engine queryTokens (internal)";
+}
+
+bool Engine::init(string applicationDir, string url)
+{
     Log(typelog::INFO) << "Engine init: " << applicationDir;
 
-    if(!fileManager.init(applicationDir))
+    if (!fileManager.init(applicationDir))
         return false;
 
-    if(!soundManager.init(isTesting))
+    if (!soundManager.init(isTesting))
         return false;
 
-    if(!cameraManager.init(url, isTesting))
+    if (!cameraManager.init(url, isTesting))
         return false;
 
     return true;
 }
 
-bool Engine::loadGame(string gameName, string lang) {
+bool Engine::loadGame(string gameName, string lang)
+{
     Log(typelog::INFO) << "Engine loadGame: " << gameName << "lang: " << lang;
     language = lang;
 
-    if(!fileManager.openVFS(gameName)) {
+    if (!fileManager.openVFS(gameName))
+    {
         Log(typelog::INFO) << "Engine loadGame - openVFS failed";
         return false;
     }
@@ -29,40 +37,45 @@ bool Engine::loadGame(string gameName, string lang) {
     return true;
 }
 
-void Engine::queryTokens(QueryTokenParam &param, QueryTokenResult &result) {
-    Log(typelog::INFO) << "Engine queryTokens (internal)";
+bool Engine::loadBoard(string boardName)
+{
+    Log(typelog::INFO) << "Engine loadBoard: " << boardName;
+    return false;
 }
 
-
-bool Engine::playSample(string sampleName) {
+bool Engine::playSample(string sampleName)
+{
     Log(typelog::INFO) << "Engine playSample: " << sampleName;
     return false;
 }
 
-
-bool Engine::playSampleSync(string sampleName, bool isLocalized) {
+bool Engine::playSampleSync(string sampleName, bool isLocalized)
+{
     Log(typelog::INFO) << "Engine playSampleSync: " << sampleName << isLocalized;
     return false;
 }
 
-bool Engine::stopAllAudio() {
+bool Engine::stopAllAudio()
+{
     Log(typelog::INFO) << "Engine stopAllAudio";
     return false;
 }
 
-bool Engine::calibrateReferenceFrame() {
+bool Engine::calibrateReferenceFrame()
+{
     Log(typelog::INFO) << "Engine calibrateReferenceFrame";
     return false;
 }
 
-bool Engine::detectColorCalibrationCard() {
+bool Engine::detectColorCalibrationCard()
+{
     Log(typelog::INFO) << "Engine calibrateReferenceFrame";
     return false;
 }
 
-string Engine::queryTokens(string json) {
+string Engine::queryTokens(string json)
+{
     Log(typelog::INFO) << "Engine queryTokens" << json;
 
     return "{ \"error\": \"\" }";
 }
-
